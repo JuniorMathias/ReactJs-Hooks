f3fce3b4f2b16ce4e53be50934aa530aa11c9a02
junior <juniormathiias@gmail.com>
2023-06-04 20:15:50 -0300

    added some functions like delete, some icons and styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
-import {Container,Form, SubmitButton} from './styles';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import React, {useState, useCallback} from 'react';
+import {Container,Form, SubmitButton, List,DeleteButton} from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+
+import api from '../../services/api';
+
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function handleInputChange(e){
+    setNewRepo(e.target.value);
+  }
+   const handleDelete = useCallback((repo) => {
+     //verifica se é diferente do nome e retorna somente os diferentes dentro do find 
+    const find = repositorios.filter( r => r.name !== repo);
+    //retorna todos os valores diferentes do repo
+    setRepositorios(find);
+   }, [repositorios]);
+
+  const handleSubmit = useCallback((e)=>{
+    e.preventDefault();
+
+    async function submit(){ 
+      setLoading(true);
+      try{
+        const response = await api.get(`repos/${newRepo}`);
+  
+        const data = {
+          name: response.data.full_name,
+        }
+      
+        setRepositorios([...repositorios, data]);
+        setNewRepo('');
+      
+      }catch(e){
+        console.log(e);
+      }finally{
+        setLoading(false);
+      }
+      }
+      
+    submit();
+
+  }, [newRepo, repositorios]);
+
+
   return (
     <Container >
       <h1>
@@ -10,13 +54,38 @@ export default function Main() {
         Meus Repositorios 
       </h1>
 
-      <Form onSubmit={() => {} }>
-        <input type="text" placeholder="Adicionar Repositorios" />
+      <Form onSubmit={handleSubmit}>
+        <input
+         type="text"
+         placeholder="Adicionar Repositorios"
+         value={newRepo}
+         onChange={handleInputChange}
+        />
 
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
+        <SubmitButton Loading={loading ? 1 : 0} >
+          {loading 
+            ? (
+              <FaSpinner color='#fff' size={14} /> 
+            ) : (
+              <FaPlus color="#fff" size={14} />
+          )}
         </SubmitButton>
       </Form>
+      <List>
+        {repositorios.map(repo => (
+          <li key={repo.name}>
+            <span>
+              <DeleteButton onClick={()=> handleDelete(repo.name)}>
+                <FaTrash size={14} />
+              </DeleteButton>
+              {repo.name}
+            </span>
+            <a href="">
+              <FaBars size={20}/>
+            </a>
+          </li>
+        ))}
+      </List>
     </Container>
   );
 }
